4a9f6c5fac07049bcb8c5e01eb83ebfd8ed1caf9
S-Sagalov <s.sagalov@mail.ru>
2023-05-06 15:15:57 +0500

    second iteration

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.3.131', '127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': {
